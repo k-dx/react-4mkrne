be5fb11b58ed40256d9d2d449adc2aa24ce17010
k-dx <tornadowro@gmail.com>
2022-06-03 20:59:15 +0200

    deleted unused code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,18 @@
 import React, { useState } from 'react';
 import './style.css';
 
-import useGenerateRandomColor from './useGenerateRandomColor';
-import { ColorContext } from './ColorContext';
 import { SelectorsContext } from './SelectorsContext';
 import ColorList from './ColorList';
 
 export default function App() {
-  // const arr = ['this', 'that', 'apple'];
-  // const arr2 = [{}, {}, {}];
-  // const COLORS = ['58355e', 'e03616', 'fff689', 'cfffb0', '5998c5'];
-
-  const [colors, setColors] = useState(
-    Array.apply(null, Array(10)).map((x, i) => '000000')
-  );
-
   const [selectors, setSelectors] = useState([null, null]);
-
-  // [
-  //   '58355e',
-  //   'e03616',
-  //   'fff689',
-  //   'cfffb0',
-  //   '5998c5',
-  // ]
 
   return (
     <div>
       <SelectorsContext.Provider value={{ selectors, setSelectors }}>
-        <ColorContext.Provider value={{ colors, setColors }}>
-          <ColorList />
-          <button onClick={() => setColors(colors.slice().reverse())}>
-            Reverse colors
-          </button>
-          <button
-            onClick={() =>
-              setColors(Array.apply(null, Array(10)).map((x, i) => '000000'))
-            }
-          >
-            Reset colors
-          </button>
-          <pre>{JSON.stringify(selectors)}</pre>
-        </ColorContext.Provider>
+        <ColorList />
+        <button onClick={() => setSelectors([null, null])}>Reset colors</button>
+        <pre>{JSON.stringify(selectors)}</pre>
       </SelectorsContext.Provider>
     </div>
   );
--- src/ListItemComponent.js
+++ src/ListItemComponent.js
@@ -1,28 +1,10 @@
 import React, { useContext } from 'react';
-import { ColorContext } from './ColorContext';
 import { SelectorsContext } from './SelectorsContext';
 
-const ListItemComponent = (id, initColor) => {
-  // const { color, generateColor } = useGenerateRandomColor(initColor);
-
-  const { colors, setColors } = useContext(ColorContext);
+const ListItemComponent = (id) => {
   const { selectors, setSelectors } = useContext(SelectorsContext);
-
-  // I _CANNOT_ use setState inside functions, right?
-  // const changeColor = (val, set) => {
-  //   console.log(val);
-  //   let newColors = ['58355e', 'e03616', 'fff689', 'cfffb0', '5998c5'];
-  //   newColors[id] = 'FF06B7';
-  //   set(newColors);
-  // };
-
   return (
     <li
-      // onClick={() => {
-      //   setColors(colors.map((e, i) => (i == id ? 'ff06b7' : e)));
-      // }}
-      // style={{ color: '#' + colors[id] }}
-
       onClick={() => {
         if (selectors[0] === null || selectors[1] !== null) {
           setSelectors([id, null]);
